67d673403ef741e5290ebabde16607c1947efd14
TheExiledCat <43574582+TheExiledCat@users.noreply.github.com>
2019-09-17 14:27:52 +0200

    arrays done

--- html/02myArray/script.js
+++ html/02myArray/script.js
@@ -6,26 +6,59 @@ const height = window.innerHeight;
 
 canvas.width = width;
 canvas.height = height;
+let points=[];
 
-let point = new Point(new Vector2d(200,300),100);
 let mouseVector = new Vector2d(0,0);
-let difference = new Vector2d(0,0);
 
+let index=0;
 
-point.draw(context);
+newList();
+ 
 
+function newList(){
+ context.clearRect(0,0,canvas.width,canvas.height);
+  for(i=0;i<10;i++){
+   
+    points[i] = new Point(new Vector2d(getRandom(canvas.width),getRandom(canvas.height)),50,"rgb(255,0,0)");
+    points[i].draw(context);
+  }
+  
+}
+console.log(points.length);
 window.addEventListener('click',(evt)=>{
   mouseVector.dx = evt.clientX;
   mouseVector.dy = evt.clientY;
   console.log(mouseVector);
-  console.log(point.position);
-  difference.differenceVector(point.position,mouseVector);
-  console.log(difference)
-  console.log(difference.magnitude);
-  if(difference.magnitude<= point.radius){
-    point.position.dx += 50;
-    point.position.dy += 50;
-    point.draw(context);
+  
+  for(i=0;i<points.length;i++){
+    let difference = new Vector2d(0,0);
+    difference.differenceVector(points[i].position,mouseVector);
+    
+    if(difference.magnitude<=50){
+      console.log("HIT");
+      if(points[i].active==false){
+        index++;
+        if(index==10){
+          newList();
+          index=0;
+          break;
+        }
+        console.log(index);
+      }
+      
+      points[i].active=true;
+      points[i].Color="rgb(0,0,255)";
+      points[i].draw(context);
+     
+    }
+    
   }
+  
+  
+    
+  
 
 })
+function getRandom(max){
+  return Math.floor(Math.random()*max);
+}
